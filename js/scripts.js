3be8c2d23095adbda5451d2a6ea92f0be0268e97
KarlStarkweather <stark13@usa.net>
2021-11-03 20:07:31 -0700

    add branches for each animal, making the one selected visible and making all others invisible when the select control is changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
+// Moose
 $(document).ready(function() {
   $("#choose-animal").change(function() {
     const animal = $("#choose-animal").val();
+    $("#Moose").hide();
+    $("#Iguana").hide();
+    $("#Turkey").hide();
+    $("#Aardvark").hide();
+
+    // $("#"+animal).show();
     if (animal === "Moose") {
-      $("#Moose").toggle();  
+      $("#Moose").show();  
+    } else if (animal === "Iguana") {
+      $("#Iguana").show();
+    } else if (animal === "Turkey") {
+      $("#Turkey").show();
+    } else if (animal === "Aardvark") {
+      $("#Aardvark").show();
     }
   });
 });
-$(document).ready(function() {
-  $("#choose-animal").change(function() {
-    const animal = $("#choose-animal").val();
-    if (animal === "#Iguana") {
-      $("#Iguana").toggle();  
