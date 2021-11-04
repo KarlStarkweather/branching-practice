cd67eaf4a1c7c040db962a917a0b7c55dc018c8b
KarlStarkweather <stark13@usa.net>
2021-11-03 19:52:21 -0700

    create div sections for other animals with images and short descriptions

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,8 @@ $(document).ready(function() {
     }
   });
 });
+$(document).ready(function() {
+  $("#choose-animal").change(function() {
+    const animal = $("#choose-animal").val();
+    if (animal === "#Iguana") {
+      $("#Iguana").toggle();  
